7c6061ec00727c75a526757d355474310b565405
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-04-12 15:46:46 +0900

    project dolharu models.py

--- dolharu/settings.py
+++ dolharu/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'bookings',
+    'staies',
+    'users',
 ]
 
 MIDDLEWARE = [
