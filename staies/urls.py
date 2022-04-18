66787fd22bbba3bb3a6047b153c6bc31198f0b7f
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2022-04-18 14:26:41 +0900

    wip

--- staies/urls.py
+++ staies/urls.py
@@ -2,5 +2,6 @@ from django.urls  import path
 from staies.views import StayDetailView
 
 urlpatterns = [
-    path('/<int:stay_id>' ,StayDetailView.as_view())
+    path('/<int:stay_id>' ,StayDetailView.as_view()),
+    path('/hosting' , HostingView.as_view())
 ]
